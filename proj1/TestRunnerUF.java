26160182b5c29d662620530962627cb950bbc976
Sky King <skyking@gmail.com>
2013-09-07 18:21:04 -0700

    UF test harness

--- proj1/TestRunnerUF.java
+++ proj1/TestRunnerUF.java
@@ -2,8 +2,10 @@
 public class TestRunnerUF {
 
     public static void main(final String[] args) {
-        final int N = StdIn.readInt();
-        final UF uf = new UF(N);
+        //final int N = StdIn.readInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(10);
+
+        /*
         while (!StdIn.isEmpty()) {
             final int p = StdIn.readInt();
             final int q = StdIn.readInt();
@@ -12,5 +14,31 @@ public class TestRunnerUF {
                 StdOut.println(p + " " + q);
             }
         }
+        */
+
+        // 3-9 2-7 2-5 5-0 2-9 4-7
+        /*
+        uf.union(3, 9);
+        uf.union(2, 7);
+        uf.union(2, 5);
+        uf.union(5, 0);
+        uf.union(2, 9);
+        uf.union(4, 7);
+        */
+
+        // 4-0 8-9 2-5 7-3 7-6 0-8 3-5 3-1 0-3
+        uf.union(4, 0);
+        uf.union(8, 9);
+        uf.union(2, 5);
+        uf.union(7, 3);
+        uf.union(7, 6);
+        uf.union(0, 8);
+        uf.union(3, 5);
+        uf.union(3, 1);
+        uf.union(0, 3);
+
+        uf.find(3);
+
+
     }
 }
